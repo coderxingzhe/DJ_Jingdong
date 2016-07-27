0dfb5f1ab1fffe720099143f54d8d0a8e9687022
段自强v <2287308986@qq.com>
2016-07-27 14:10:15 +0800

    1初始化项目

---
+++ DJ_jingdong/DJ_jingdong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DJ_jingdong
+//
+//  Created by Mac on 16/7/27.
+//  Copyright © 2016年 DJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DJ_jingdong/DJ_jingdong/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DJ_jingdong
+//
+//  Created by Mac on 16/7/27.
+//  Copyright © 2016年 DJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
